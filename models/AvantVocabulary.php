77071b08ef8f09e83a49cb33ce937eebc59e6b31
gsoules <gsoules@avantlogic.com>
2020-06-10 12:18:02 -0400

    Code refactoring.

--- models/AvantVocabulary.php
+++ models/AvantVocabulary.php
@@ -19,17 +19,27 @@ class AvantVocabulary
     // Common terms with an Id higher than this do not come from Nomenclature 4.0.
     const VOCABULARY_FIRST_NON_NOMENCLATURE_COMMON_TERM_ID = 20000;
 
+    public static function getVocabularyFields()
+    {
+        return array(
+            'Type'=>AvantVocabulary::VOCABULARY_TERM_KIND_TYPE,
+            'Subject'=>AvantVocabulary::VOCABULARY_TERM_KIND_SUBJECT,
+            'Place'=>AvantVocabulary::VOCABULARY_TERM_KIND_PLACE
+        );
+    }
+
     public static function getVocabularyKinds()
     {
         // Return a table that associates element Ids with their vocabulary kind. The element Id
         // cannot be used as the kind since it could vary on different Digital Archive installations.
         $kindTable = [];
-        $typeElementId = ItemMetadata::getElementIdForElementName('Type');
-        $subjectElementId = ItemMetadata::getElementIdForElementName('Subject');
-        $placeElementId = ItemMetadata::getElementIdForElementName('Place');
-        $kindTable[$typeElementId] = AvantVocabulary::VOCABULARY_TERM_KIND_TYPE;
-        $kindTable[$subjectElementId] = AvantVocabulary::VOCABULARY_TERM_KIND_SUBJECT;
-        $kindTable[$placeElementId] = AvantVocabulary::VOCABULARY_TERM_KIND_PLACE;
+        $vocabularyFields = self::getVocabularyFields();
+        foreach ($vocabularyFields as $fieldName => $kind)
+        {
+            $elementId = ItemMetadata::getElementIdForElementName($fieldName);
+            $kindTable[$elementId] = $kind;
+        }
+
         return $kindTable;
     }
 
