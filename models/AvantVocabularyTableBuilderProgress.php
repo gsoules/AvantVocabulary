0bd167f01d1791030619eeb770282a67ba7a8fc7
gsoules <gsoules@avantlogic.com>
2020-06-12 09:32:19 -0400

    Catch an exception that occurs during progress reporting.

--- models/AvantVocabularyTableBuilderProgress.php
+++ models/AvantVocabularyTableBuilderProgress.php
@@ -17,28 +17,46 @@ class AvantVocabularyTableBuilderProgress
 
     protected function getProgress($tableName)
     {
-        if ($tableName == 'common')
+        try
         {
-            $count = $this->db->getTable('VocabularyCommonTerms')->getRowCount();
-            $count = number_format($count, 0, '.', ',');
-            $message = $count . __(' records written to the common terms table');
-        }
-        else if ($tableName == 'local')
-        {
-            $count = $this->db->getTable('VocabularyLocalTerms')->getRowCount();
-            $count = number_format($count, 0, '.', ',');
-            $message = $count . __(' records written to the local terms table');
-        }
-        else
-        {
-            $progressFileName = AvantVocabulary::progressFileName();
-            $message = 'Updating items in database: ';
-            if (file_exists($progressFileName))
-                $progress = file_get_contents($progressFileName);
+            if ($tableName == 'common')
+            {
+                $count = $this->db->getTable('VocabularyCommonTerms')->getRowCount();
+                $count = number_format($count, 0, '.', ',');
+                $message = $count . __(' records written to the common terms table');
+            }
             else
-                $progress = '0%';
-            $message .= $progress;
+            {
+                if ($tableName == 'local')
+                {
+                    $count = $this->db->getTable('VocabularyLocalTerms')->getRowCount();
+                    $count = number_format($count, 0, '.', ',');
+                    $message = $count . __(' records written to the local terms table');
+                }
+                else
+                {
+                    $progressFileName = AvantVocabulary::progressFileName();
+                    $message = 'Updating items in database: ';
+                    if (file_exists($progressFileName))
+                    {
+                        $progress = file_get_contents($progressFileName);
+                    }
+                    else
+                    {
+                        $progress = '0%';
+                    }
+                    $message .= $progress;
+                }
+            }
         }
+        catch (Exception $e)
+        {
+            // This should not happen under normal circumstances, but it's possible for this method to get called
+            // in between the time when a table is dropped and when it gets created again. In that situation,
+            // an exception would occur when attempting to read the table.
+            $message = "Unable to report progress: " . $e->getMessage();
+        }
+
         return $message;
     }
 }
